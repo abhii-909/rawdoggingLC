ad32b0ce44efc1ad0eefd81fc416c2c7f7ceaf5d
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2025-03-28 09:34:11 +0530

    Time: 3 ms (71.17%) | Memory: 16.8 MB (10.70%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
     vector<string> result;
-    bool isValid(string str){
-        int count = 0;
-        for(char ch : str){
-            if(ch == '(')
-                count++;
-            else
-                count--;
-            if(count < 0)
-                return false;
-        }
-        return count == 0;
-    }
-    void solve(string curr, int n){
+    
+    void solve(string curr, int n, int open, int close){
         if(curr.length() == 2*n){
-            if(isValid(curr)){
-                result.push_back(curr);
-            }
+            result.push_back(curr);
             return;
         }
         //right travel
-        curr.push_back('(');
-        solve(curr, n);
-
-        curr.pop_back();
+        if(open < n){
+            curr.push_back('(');
+            solve(curr, n, open+1, close);
+            curr.pop_back();
+        }
 
         //left travel
-        curr.push_back(')');
-        solve(curr, n);
-        curr.pop_back();
+        if(close < open){
+            curr.push_back(')');
+            solve(curr, n, open, close+1);
+            curr.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         string curr = "";
+        int open = 0;
+        int close = 0;
 
-        solve(curr, n);
+        solve(curr, n, open, close);
 
         return result;
     }
