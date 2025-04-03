e1cbe514521eda6c20df2af6e364be7fd6fa9dce
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2025-04-03 09:57:24 +0530

    Time: 15 ms (10.79%) | Memory: 8.7 MB (14.70%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans = 1;
+        for(long long i = 0; i <= x; i++){
+            if(i*i <= x){
+                ans = i;
+            }else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
