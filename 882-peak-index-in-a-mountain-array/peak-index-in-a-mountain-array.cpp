8f4e1ab1ea2a02852caec3e8b2e49b7b1922c96e
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2025-04-08 11:55:24 +0530

    Time: 0 ms (100.00%) | Memory: 63.4 MB (78.94%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int left = 1;
-        int right = arr.size() - 2;
+        int left = 0;
+        int right = arr.size() - 1;
         while (left < right) {
             int mid = (left + right) >> 1;
-            
+
             if (arr[mid] > arr[mid + 1])
                 right = mid;
             else
