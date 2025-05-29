89bc895cdcf7aa8098c00f2a8a1ad995b24fe5b0
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2025-05-29 17:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (24.16%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int majority = nums[0];
-        int count = 1;
-        
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i] == majority) {
-                count++;
-            } else if (count > 0) {
-                count--;
-            } else {
-                majority = nums[i];
-                count = 1;
-            }
+        //Moore's Voting algo
+        int freq = 0, ans = 0;
+
+        for(int val : nums){
+            if(freq == 0) ans = val;
+            if(ans == val) freq++;
+            else freq--;
         }
-        
-        return majority;
+        return ans;
     }
 };
