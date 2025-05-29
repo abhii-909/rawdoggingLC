ca6325018ccc486af15c04c068dd8d5e112fb9a4
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2025-05-29 18:30:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.69%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long N = n; // handle edge case when n == INT_MIN
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
+        if(n == 0) return 1.0;
+        if(x == 0) return 0.0;
+        if(x == 1) return 1.0;
+        if(x == -1 && n%2 == 0) return 1.0;
+        if(x == -1 && n%2 != 0) return -1.0;
+        
+        long long binForm = n;
+        if(n < 0){
+            x = 1/x;
+            binForm = -binForm;
         }
-        double result = 1.0;
-        while (N > 0) {
-            if (N % 2 == 1) {
-                result *= x;
+
+        double ans = 1;
+
+        while(binForm > 0){
+            if(binForm % 2 == 1){
+                ans *= x;
             }
             x *= x;
-            N /= 2;
+            binForm /= 2;
         }
-        return result;
+        return ans;
     }
 };
