072c426c0734def53a46990131cfbb3d67e95590
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2025-07-07 19:26:59 +0530

    Time: 60 ms (69.12%) | Memory: 75.1 MB (31.56%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-        // sort events by start day
         sort(events.begin(), events.end());
 
-        // min-heap for end days
         priority_queue<int, vector<int>, greater<int>> minHeap;
 
         int i = 0, cnt = 0;
         int n = events.size();
 
-        // find the last day to simulate
         int lastDay = 0;
         for(const auto& e : events) lastDay = max(lastDay, e[1]);
 
-        // simulate each day
         for(int day = 1; day <= lastDay; day++){
-            // add all events starting today
             while(i < n && events[i][0] == day){
                 minHeap.push(events[i][1]);
                 i++;
             }
-            // remove expired events
             while(!minHeap.empty() && minHeap.top() < day){
                 minHeap.pop();
             }
-            // attend one event with earliest end
             if(!minHeap.empty()){
                 minHeap.pop();
                 cnt++;
             }
-            // early stop
             if(i == n && minHeap.empty()) break;
         }
         return cnt;
