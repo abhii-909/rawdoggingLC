ef9482efdf3ce57ceae18bde3be55921b57e6abc
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2025-07-23 10:26:18 +0530

    Time: 20 ms (93.79%) | Memory: 19.7 MB (97.00%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -1,53 +1,34 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        // Step 1: Remove higher-value substring first
-        if (x < y)
-        {
-            return remove(s, 'b', 'a', y, x); // remove "ba" first
-        }
-        else
-        {
-            return remove(s, 'a', 'b', x, y); // remove "ab" first
-        }
-    }
+        int a = 0;
+        int b = 0;
+        int l = min(x, y);
+        int r = 0;
 
-private:
-    int remove(string& s, char first, char second, int firstPoints, int secondPoints) 
-    {
-        int score = 0;
-        vector<char> stack;
-
-        // Step 2: First pass - remove the higher-value substring
-        for (char c : s) 
-        {
-            if (!stack.empty() && stack.back() == first && c == second) 
-            {
-                stack.pop_back();
-                score += firstPoints;
-            } 
-            else 
-            {
-                stack.push_back(c);
+        for (char c : s) {
+            if (c > 'b') {
+                r += min(a, b) * l;
+                a = 0;
+                b = 0;
+            } else if (c == 'a') {
+                if (x < y && b > 0) {
+                    b--;
+                    r += y;
+                } else {
+                    a++;
+                }
+            } else {
+                if (x > y && a > 0) {
+                    a--;
+                    r += x;
+                } else {
+                    b++;
+                }
             }
         }
 
-        // Step 3: Second pass - remove the lower-value substring from remaining string
-        vector<char> stack2;
-        for (char c : stack) 
-        {
-            if (!stack2.empty() && stack2.back() == second && c == first) 
-            {
-                stack2.pop_back();
-                score += secondPoints;
-            } 
-            else 
-            {
-                stack2.push_back(c);
-            }
-        }
-
-        // Step 4: Return total score
-        return score;
+        r += min(a, b) * l;
+        return r;
     }
 };
