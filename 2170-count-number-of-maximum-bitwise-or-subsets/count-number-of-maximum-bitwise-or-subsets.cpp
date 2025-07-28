b996cc2b0ed82f0775d239476e79b64ab1f9dd31
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2025-07-28 09:30:02 +0530

    Time: 6 ms (94.44%) | Memory: 10.7 MB (66.06%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
     int countMaxOrSubsets(vector<int>& nums) {
-        // Step 1: Find the maximum possible OR value
-        // This is achieved by doing OR of all elements
         int maxOr = 0;
         for (int num : nums) {
             maxOr |= num;
         }
-        
-        // Step 2: Count subsets that achieve this maximum OR
+
         int count = 0;
         backtrack(nums, 0, 0, maxOr, count);
-        
         return count;
     }
     
 private:
     void backtrack(vector<int>& nums, int index, int currentOr, int targetOr, int& count) {
-        // Base case: we've considered all elements
         if (index == nums.size()) {
-            // If current subset's OR equals target, increment count
             if (currentOr == targetOr) {
                 count++;
             }
             return;
         }
-        
-        // Choice 1: Include current element in subset
-        // Update OR with current element and move to next
         backtrack(nums, index + 1, currentOr | nums[index], targetOr, count);
-        
-        // Choice 2: Skip current element (don't include in subset)
-        // Keep OR same and move to next element
         backtrack(nums, index + 1, currentOr, targetOr, count);
     }
 };
