1027e229a662fca31518c2e922be99e3bfe4f3a1
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2025-08-29 23:40:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.03%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++){
-            int j = i;
-            while(j>0 && nums[j-1] > nums[j]){
-                int temp = nums[j];
-                nums[j] = nums[j-1];
-                nums[j-1] = temp;
-                j--;
+        int n = nums.size();
+
+        int i = 0;     // for 0
+        int j = 0;     // for 1
+        int k = n-1;   // for 2 (n-1 isliye kyuki 2 sabse bada hai isliye last mei hi hoga humesha)
+
+        while(j <= k){
+            if(nums[j] == 1) j++;  // j happy kyuki j ko 1 chaiye aur milgaya
+
+            else if(nums[j] == 2){
+                swap(nums[j], nums[k]);   // k aur j mei rola chidh gaya kyuki k ko 2 chaiye
+                k--;
+            }else{ // nums[j] == 0
+                swap(nums[j], nums[i]);   // ab i bolega 0 mere pass hona chaiye
+                i++;
+                j++;     // j ko ++ kyuki i and j started at the same place
             }
         }
     }
