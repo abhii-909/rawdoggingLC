8d8b89bc4382206560880f949e471f00ea76e509
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2025-09-16 11:08:08 +0530

    Time: 22 ms (83.63%) | Memory: 75.2 MB (18.99%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,51 +1,22 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int n = fruits.size();
-        int b1 = -1, b2 = -1;
-        int cnt1 = 0, cnt2 = 0;
-        int ans = 0, left = 0;
+        int left = 0, maxLen = 0;
+        unordered_map<int,int> basket;
+        
+        for(int right =0; right < fruits.size(); right++){
+            basket[fruits[right]]++;                       // curr fruit ko basket mei daal do
 
-        // sliding window approach
-        for(int right = 0; right < n; right++){
-            // fruit 1
-            if(b1 == fruits[right]){
-                cnt1++;
-            }
-            // fruit 2
-            else if(b2 == fruits[right]){
-                cnt2++;
-            }
-            // baskets are empty
-            else if(b1 == -1){
-                b1 = fruits[right];
-                cnt1++;
-            }else if(b2 == -1){
-                b2 = fruits[right];
-                cnt2++;
-            }
-            // third type
-            else{
-                while(cnt1 > 0 && cnt2 > 0){
-                    if(b1 == fruits[left]){
-                        cnt1--;
-                    }else if(b2 == fruits[left]){
-                        cnt2--;
-                    }
-                    left++;
+            // 2 se zyada types aane par shrink kardenge window ko
+            while(basket.size() > 2){
+                basket[fruits[left]]--;
+                if(basket[fruits[left]] == 0){
+                    basket.erase(fruits[left]);
                 }
-                // whatever fruit is discarded we replace it's basket with the new type:arr[right]
-                if(cnt1 == 0){
-                    b1 = fruits[right];
-                    cnt1++;
-                }else if(cnt2 == 0){
-                    b2 = fruits[right];
-                    cnt2++;
-                }
+                left++;
             }
-            // the current window size will be the answer
-            ans = max(ans, right - left + 1);
+            maxLen = max(maxLen, right - left + 1);
         }
-        return ans; // max size found
+        return maxLen;
     }
 };
