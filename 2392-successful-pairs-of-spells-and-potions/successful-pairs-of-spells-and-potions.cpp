c3f48af8e8ccdd95cd1d58a06b78845ce13a581c
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2025-10-08 19:58:55 +0530

    Time: 38 ms (86.98%) | Memory: 125 MB (81.16%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -8,15 +8,11 @@ public:
         ans.reserve(spells.size());
 
         for(int e : spells){
-            // potion * spell >= success => potion >= success/spell;
             ll need = (success + e - 1) / e;
 
-            // find the first potion >= need using binary search
             auto it = lower_bound(potions.begin(), potions.end(), need);
-            // counts how many steps from potions.begin() to it
             int idx = it - potions.begin();
 
-            // count how many potions from idx to end are valid
             int cnt = potions.size() - idx;
             ans.push_back(cnt);
         }
