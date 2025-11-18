e40caac2de203d1e358ec0733827c927eefa0f96
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2025-11-18 09:43:23 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (72.06%) - LeetSync

---
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+        int cnt = 0;
+
+        for(int i = n-2; i >= 0 && bits[i] == 1; --i){
+            ++cnt;
+        }
+        bool even = cnt % 2 == 0;
+        return even;
+    }
+};
