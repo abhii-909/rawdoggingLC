ce2758cc261fb45a1d7464c773253cb4fdc5df74
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2025-12-04 10:39:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,67 @@
+// PLAIN RECURSION  --> TLE
+
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n <= 1) return 1;
+//         return climbStairs(n-1) + climbStairs(n-2);
+//     }
+// };
+
+// M1  --->   TOP-DOWN DP
+
+// class Solution {
+// public:
+//     int solve(int n, vector<int>& dp){
+//         if(n <= 1) return 1;
+
+//         if(dp[n] != -1) return dp[n];
+
+//         return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+//     }
+
+//     int climbStairs(int n) {
+//         vector<int> dp(n+1, -1);
+//         return solve(n, dp);
+//     }
+// };
+
+
+// M2  ----> BOTTOM-UP DP
+
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n <= 1) return 1;
+
+//         vector<int> dp(n+1, -1);
+//         dp[0] = 1;
+//         dp[1] = 1;
+
+//         for(int i = 2; i <= n; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// };
+
+
+// Space Optimized DP (Two Variables)
+
 class Solution {
 public:
-    int solve(int n, vector<int>& dp){
+    int climbStairs(int n) {
         if(n <= 1) return 1;
 
-        if(dp[n] != -1) return dp[n];
+        int prev2 = 1;  
+        int prev1 = 1;  
 
-        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
-    }
+        for(int i = 2; i <= n; i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
 
-    int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        return prev1;
     }
 };
