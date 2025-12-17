f7a01b224554ad4a284df9bb1865df884e47838c
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2025-12-17 10:33:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (49.82%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,23 +1,31 @@
-// Memoiozation
+// Tabulation
 
 class Solution {
 public:
-    int solve(int i, int j, int m, int n, vector<vector<int>>& t){
-
-        if(i == m-1 && j == n-1) return 1;  // found the path
-
-        if(i < 0 || i >= m || j < 0 || j >= n) return 0; // out of bounds
-
-        if(t[i][j] != -1) return t[i][j];
-
-        int r = solve(i, j+1, m , n, t);
-        int d = solve(i+1, j, m , n, t);
-
-        return t[i][j] = r + d;
-    }
-
     int uniquePaths(int m, int n) {
-        vector<vector<int>> t(m+1, vector<int>(n+1, -1));
-        return solve(0, 0, m, n, t);
+        vector<vector<int>> t(m, vector<int> (n));
+        //t[i][j] = total no. of ways to reach [i][j] from [0][0]
+
+        t[0][0] = 1;
+
+        // fill 0th row
+        for(int col = 1; col < n; col++){
+            t[0][col] = 1;
+        }
+
+        // fill 0th col
+        for(int row = 1; row < m; row++){
+            t[row][0] = 1;
+        }
+
+        // fill baaki cells
+        for(int i = 1; i < m; i++){
+
+            for(int j = 1; j < n; j++){
+                t[i][j] = t[i-1][j] + t[i][j-1];
+            }
+        }
+
+        return t[m-1][n-1];
     }
 };
