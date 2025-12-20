42a2a889d0652e35cc35c1a70818c6fbd753924e
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2025-12-20 22:52:32 +0530

    Time: 11 ms (5.09%) | Memory: 18.3 MB (24.99%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
+    typedef pair<int, string> P;
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string, int> mp;
-        //step 1 : store frequency
+
         for(auto &word : words){
             mp[word]++;
         }
 
-        vector<pair<string, int>> v;
-        //step 2 : fill with {word, freq}
-        for(auto &it : mp){
-            v.push_back({it.first, it.second});
-        }
-        //step 3 : sort the vector with comparator
-        auto lamda = [](pair<string, int> p1, pair<string, int> p2){
-            if(p1.second == p2.second){
-                return p1.first < p2.first;
+        auto lamda = [](const P &a, const P &b) {
+            if (a.first == b.first){
+                return a.second < b.second;
             }
-            return p1.second > p2.second;
+            return a.first > b.first;
         };
-        sort(v.begin(), v.end(), lamda);
+        priority_queue<P, vector<P>, decltype(lamda)> pq(lamda);   //min heap
 
-        //step 4 : top k ko utha lo
-        int i = 0;
+        for(auto &it : mp){
+            string val = it.first;
+            int freq = it.second;
+
+            pq.push({freq, val});
+
+            if(pq.size() > k) pq.pop();
+        }
+
         vector<string> ans;
 
-        while(i < k){
-            ans.push_back(v[i].first);
-            i++;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
