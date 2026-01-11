a0c7fac1727be340e86502ec83c1e6f13594cde3
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2026-01-11 10:40:07 +0530

    Time: 31 ms (53.71%) | Memory: 19 MB (53.50%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,25 +1,57 @@
+//RECURSION
+
+// class Solution {
+// public:
+//     int solve(vector<int>& coins, int amount){
+//         if(amount == 0) return 0;   // no coins needed
+//         if(amount < 0) return 1e9;  // invalid case
+
+//         int ans = 1e9;
+
+//         for(int x : coins){
+//             ans = min(ans, 1 + solve(coins, amount - x));
+//         }
+//         return ans;
+//     }
+
+//     int coinChange(vector<int>& coins, int amount) {
+//         int res = solve(coins, amount);
+
+//         return res >= 1e9 ? -1 : res;
+//     }
+// };
+
+//__________________________________________________________________________
+
+//MEMOIZATION
+
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        // dp[i] will be storing the minimum number of coins required for amount i.
-        // We use amount + 1 as an "infinity" value.
-        vector<int> dp(amount + 1, amount + 1);
-        
-        // Base case: 0 coins are needed for amount 0.
-        dp[0] = 0;
-        
-        // Iterate through all amounts from 1 to the target amount.
-        for (int i = 1; i <= amount; ++i) {
-            // For each amount, check each coin.
-            for (int coin : coins) {
-                if (i - coin >= 0) {
-                    // If we can use this coin, update the dp table for the current amount.
-                    dp[i] = min(dp[i], 1 + dp[i - coin]);
-                }
-            }
+    vector<int> t;
+
+    int solve(vector<int>& coins, int amount){
+
+        if(amount == 0) return 0;   // no coins needed
+        if(amount < 0) return 1e9;  // invalid case
+
+        if(t[amount] != -1){
+            return t[amount];
         }
+
+        int ans = 1e9;
+
+        for(int x : coins){
+            ans = min(ans, 1 + solve(coins, amount - x));
+        }
+        return t[amount] = ans;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+
+        t.assign(amount + 1, -1);
         
-        // If dp[amount] is still the "infinity" value, it means the amount cannot be formed.
-        return dp[amount] > amount ? -1 : dp[amount];
+        int res = solve(coins, amount);
+
+        return res >= 1e9 ? -1 : res;
     }
 };
