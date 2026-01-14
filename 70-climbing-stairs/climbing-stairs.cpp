398cd4c6e4dda6918bbee6fe964983106adad3c4
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2026-01-14 16:34:09 +0530

    Time: 1 ms (5.48%) | Memory: 8.7 MB (16.85%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -17,25 +17,49 @@
 
 //MEMOIZATION
 
+// class Solution {
+// public:
+//     vector<int> dp;
+
+//     int solve(int n)
+//     {
+//         if(n < 0) return 0;
+//         if(n == 0) return 1;
+
+//         if(dp[n] != -1) return dp[n];
+
+//         int one_step = solve(n-1);
+//         int two_steps = solve(n-2);
+
+//         return dp[n] = one_step + two_steps;
+//     }
+
+//     int climbStairs(int n) {
+//         dp.assign(n+1, -1);
+//         return solve(n);
+//     }
+// };
+
+//_____________________________________________________________________________________________
+
+// TABULATION
+
 class Solution {
 public:
-    vector<int> dp;
-
-    int solve(int n)
-    {
-        if(n < 0) return 0;
-        if(n == 0) return 1;
-
-        if(dp[n] != -1) return dp[n];
-
-        int one_step = solve(n-1);
-        int two_steps = solve(n-2);
-
-        return dp[n] = one_step + two_steps;
-    }
-
     int climbStairs(int n) {
-        dp.assign(n+1, -1);
-        return solve(n);
+        if(n == 0) return 1;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        vector<int> dp(n+1);
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
