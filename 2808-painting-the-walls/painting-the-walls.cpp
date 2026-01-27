524ec81e5bdc9bfdf93c21790a0b0bde9f99fd66
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2026-01-27 09:14:06 +0530

    Time: 28 ms (83.73%) | Memory: 95.9 MB (65.51%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.cpp
+++ 2808-painting-the-walls/painting-the-walls.cpp
@@ -1,22 +1,16 @@
 class Solution {
-private:
-    int memo(vector<int>&cost, vector<int> &time, int index, int walls, vector<vector<int>> &dp) {
-        if(walls <= 0) return 0;//can be one of the possible way
-
-        if(index < 0 ) return 1e9;//Neglect this by returning unusually large value
-
-        if(dp[index][walls] != -1) return dp[index][walls];
-
-        int notTake = 0 + memo(cost, time, index - 1, walls, dp);
-        int take = cost[index] + memo(cost, time, index - 1, walls- time[index] - 1, dp);
-
-        return dp[index][walls] = min(notTake, take);
-    }
 public:
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        
-        return memo(cost, time, n-1, n, dp);
+        vector<long long> money(n + 1, 1e9);
+
+        money[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > 0; j--) {
+                money[j] = min(money[j], money[max(j - time[i] - 1, 0)] + cost[i]);
+            }
+        }
+        return (int)money[n];
     }
 };
