a889bb94da23fd749617c23d5fd4e276e377fbc9
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2026-02-05 11:39:47 +0530

    Time: 29 ms (42.92%) | Memory: 16.5 MB (66.61%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
+    int n, m;
+
     int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) {
-            return 0;
-        }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        if (grid.empty()) return 0;
+
+        n = grid.size();
+        m = grid[0].size();
+
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+        int islands = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    islands++;
+                    dfs(grid, visited, i, j);
                 }
             }
         }
-        
-        return numIslands;
+        return islands;
     }
-    
+
 private:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
+    void dfs(vector<vector<char>>& grid,
+             vector<vector<bool>>& visited,
+             int i, int j) {
+
+        // boundary + water + already visited
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() 
+             || grid[i][j] == '0' || visited[i][j]) {
             return;
         }
-        
-        grid[i][j] = '0';   // mark as visited
 
-        dfs(grid, i-1, j);  //up
-        dfs(grid, i, j-1); //left
-        dfs(grid, i+1, j); //down
-        dfs(grid, i, j+1); //right
+        visited[i][j] = true;
+
+        dfs(grid, visited, i - 1, j); // up
+        dfs(grid, visited, i + 1, j); // down
+        dfs(grid, visited, i, j - 1); // left
+        dfs(grid, visited, i, j + 1); // right
     }
 };
