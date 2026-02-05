d16ecdea8102dbb06baa0da8bf997c1ddb836a25
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2026-02-05 11:45:44 +0530

    Time: 17 ms (17.68%) | Memory: 104.8 MB (22.50%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-int n, m;
-int perimeter = 0;
+    int n, m;
+    int perimeter = 0;
 
-void dfs(vector<vector<int>> &grid, int i, int j){
-    // out of bounds
-    if(i >= n || j >= m || i < 0 || j < 0 || grid[i][j] == 0){
-        perimeter++;
-        return;
+    void dfs(vector<vector<int>> &grid, vector<vector<bool>> &visited, int i, int j) {
+
+        // out of bounds OR water
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 0) {
+            perimeter++;
+            return;
+        }
+
+        // already visited land
+        if (visited[i][j]) return;
+
+        visited[i][j] = true;
+
+        dfs(grid, visited, i - 1, j); // up
+        dfs(grid, visited, i + 1, j); // down
+        dfs(grid, visited, i, j - 1); // left
+        dfs(grid, visited, i, j + 1); // right
     }
 
-    // already visited
-    if(grid[i][j] == -1) return;
-
-    // mark visited
-    grid[i][j] = -1;
-
-    dfs(grid, i-1, j);  //up
-    dfs(grid, i, j-1); //left
-    dfs(grid, i+1, j); //down
-    dfs(grid, i, j+1); //right
-}
     int islandPerimeter(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
 
-                //land
-                if(grid[i][j] == 1){
-                    dfs(grid, i, j);
-                    return perimeter;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    dfs(grid, visited, i, j);
+                    return perimeter;   // only one island
                 }
             }
         }
