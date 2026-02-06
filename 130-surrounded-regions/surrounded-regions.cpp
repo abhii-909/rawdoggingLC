8d29f64aee0ee8ffd47297d6e9d8a5bcc6e12f99
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2026-02-06 10:46:16 +0530

    Time: 1 ms (32.62%) | Memory: 13.9 MB (80.59%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -13,27 +13,32 @@ public:
 
     void solve(vector<vector<char>>& grid) {
         for(int i=0;i<grid.size();i++){
+
             if(grid[i][0]=='O'){
                 DFS(grid,i,0);
             }
         }
         for(int i=0;i<grid.size();i++){
+
             if(grid[i][grid[0].size()-1]=='O'){
                 DFS(grid,i,grid[0].size()-1);
             }
         }
         for(int i=0;i<grid[0].size();i++){
+
             if(grid[0][i]=='O'){
                 DFS(grid,0,i);
             }
         }
         for(int i=0;i<grid[0].size();i++){
+
             if(grid[grid.size()-1][i]=='O'){
                 DFS(grid,grid.size()-1,i);
             }
         }
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
+                
                 if(grid[i][j]=='O'){
                     grid[i][j]='X';
                 }
