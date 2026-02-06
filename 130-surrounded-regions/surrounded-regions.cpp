55cdd616a1d4abee5d1604caa6dd6f2a62a43722
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2026-02-06 10:45:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (89.83%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,56 +1,39 @@
 class Solution {
 public:
-    void BFS(vector<vector<char>>&grid,int i,int j){
-        int dx[]={1,-1,0,0};
-        int dy[]={0,0,1,-1};
-
-        queue<pair<int,int>>q;
-        q.push({i,j});
-
-        grid[i][j]='o';
-
-        while(!q.empty()){
-            auto [r,c]=q.front();
-            q.pop();
-
-            for(int i=0;i<4;i++){
-                int x=r+dx[i];
-                int y=c+dy[i];
-
-                if(x>=0&&x<grid.size()&&y>=0&&y<grid[0].size()&&grid[x][y]=='O'){
-                    grid[x][y]='o';
-                    q.push({x,y});
-                }
-            }
+    void DFS(vector<vector<char>>&grid,int i,int j){
+        if(i<0||i>=grid.size()||j<0||j>=grid[0].size()||grid[i][j]!='O'){
+            return;
         }
+        grid[i][j]='o';
+        DFS(grid,i+1,j);
+        DFS(grid,i-1,j);
+        DFS(grid,i,j+1);
+        DFS(grid,i,j-1);
     }
+
     void solve(vector<vector<char>>& grid) {
         for(int i=0;i<grid.size();i++){
             if(grid[i][0]=='O'){
-                BFS(grid,i,0);
+                DFS(grid,i,0);
             }
         }
         for(int i=0;i<grid.size();i++){
-
             if(grid[i][grid[0].size()-1]=='O'){
-                BFS(grid,i,grid[0].size()-1);
+                DFS(grid,i,grid[0].size()-1);
             }
         }
         for(int i=0;i<grid[0].size();i++){
-
             if(grid[0][i]=='O'){
-                BFS(grid,0,i);
+                DFS(grid,0,i);
             }
         }
         for(int i=0;i<grid[0].size();i++){
-
             if(grid[grid.size()-1][i]=='O'){
-                BFS(grid,grid.size()-1,i);
+                DFS(grid,grid.size()-1,i);
             }
         }
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                
                 if(grid[i][j]=='O'){
                     grid[i][j]='X';
                 }
