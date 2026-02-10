2c9fb6839d6c961b8cc75424d4b1a0674d8a884c
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2026-02-10 09:21:28 +0530

    Time: 56 ms (23.14%) | Memory: 66.3 MB (38.56%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,38 +1,103 @@
+// //Using DFS-cycle detection
+
+// class Solution {
+// public:
+//     bool isCycleDFS(vector<vector<int>>& adj, int u, vector<bool>& vis, vector<bool>& inRecursion) {
+//         vis[u] = true;
+//         inRecursion[u] = true;
+        
+        
+//         for(int &v : adj[u]) {
+//             //if not vis, then we check for cycle in DFS
+//             if(vis[v] == false && isCycleDFS(adj, v, vis, inRecursion)){
+//                 return true;
+//             } else if(inRecursion[v] == true){
+//                 return true;
+//             }
+//         }
+        
+//         inRecursion[u] = false;
+//         return false;
+        
+//     }
+    
+//     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+//         int V = graph.size();
+//         vector<bool> vis(V, false);
+//         vector<bool> inRecursion(V, false);
+        
+//         for(int i = 0; i < V; i++) {
+//             if(!vis[i]){
+//                 isCycleDFS(graph, i, vis, inRecursion);
+//             }
+//         }
+        
+//         vector<int> safeNodes;
+//         for(int i = 0; i < V; i++) {
+//             if(!inRecursion[i]){
+//                 safeNodes.push_back(i);
+//             }
+//         }
+        
+//         return safeNodes;
+//     }
+// };
+
+// ________________________________________________________________________________________________
+
+// USING BFS (Topological sort)
+
 class Solution {
 public:
-    int n;
-    vector<int> degreein;
-    vector< vector<int> > rGraph;
-    vector<int> ans;
-    
-    void init(vector<vector<int>>& G){
-        n = G.size();
-        degreein = vector<int>(n,0);
-        rGraph.resize(n, vector<int>(0) );
-        
-        for (int u=0; u<n; u++){
-            for (int v:G[u]){
-                rGraph[v].push_back(u);
-                degreein[u]++;
-            }
-        }
-    }
-    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        init(graph);
+        int V = graph.size();
+
+        vector<vector<int>> adj(V);
+        
+        
         queue<int> que;
-        for (int i=0; i<n; i++){
-            if (degreein[i]==0) que.push(i);
-        }
-        while (que.size()){
-            int u = que.front(); que.pop();
-            ans.push_back(u);
-            for (int v:rGraph[u]){
-                degreein[v]--;
-                if (degreein[v]==0) que.push(v);
+	    vector<int> indegree(V, 0);
+	    int count = 0;
+	    //1
+	    for(int u = 0; u < V; u++) {
+	        for(int &v : graph[u]) {
+                adj[v].push_back(u);
+	            indegree[u]++;
+	        }
+	    }
+	    
+	    //2. Fill que, indegree with 0
+	    for(int i = 0; i < V; i++) {
+	        if(indegree[i] == 0) {
+	            que.push(i);
+	            count++;
+	        }
+	    }
+	    
+	    //3. Simple BFS
+        vector<bool> safe(V, false);
+	    while(!que.empty()) {
+	        int u = que.front();
+	        que.pop();
+            safe[u] = true;
+	        
+	        for(int &v : adj[u]) {
+	            indegree[v]--;
+	            
+	            if(indegree[v] == 0) {
+	                que.push(v);
+	                count++;
+	            }
+	            
+	        }
+	    }
+	    
+	    vector<int> safeNodes;
+        for(int i = 0; i < V; i++) {
+            if(safe[i]) {
+                safeNodes.push_back(i);
             }
         }
-        sort(ans.begin(),ans.end());
-        return ans;
+        return safeNodes;
     }
 };
