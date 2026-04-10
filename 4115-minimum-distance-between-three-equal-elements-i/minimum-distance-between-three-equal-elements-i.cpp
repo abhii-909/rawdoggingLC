047e2e79f2682eb9554ae9d2fdf5da24deb985ec
Abbhinav Mishra <abbhinav99mishra@gmail.com>
2026-04-10 10:08:20 +0530

    Time: 2 ms (75.63%) | Memory: 28.7 MB (79.62%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
@@ -8,12 +8,16 @@ public:
         int ans = INT_MAX ;
         for (int i = 0; i < n; i++) {
 
-            for (int j = i+1; j < n; j++)
-                if (nums[i] == nums[j])
+            for (int j = i+1; j < n; j++){
+                if (nums[i] == nums[j]){
 
-                for (int k = j+1; k < n; k++)
-                    if (nums[j] == nums[k])
-                    ans = min(ans, 2*(k-i));
+                    for (int k = j+1; k < n; k++){
+                        if (nums[j] == nums[k]){
+                            ans = min(ans, 2*(k-i));
+                        }
+                    }
+                }
+            }
         }
         if (ans == INT_MAX) return -1;
         else return ans ;
